a6603c97ed50cb3e870c958c26a9329bf51af9c4
Loick01 <loicker89@gmail.com>
2026-01-28 17:08:50 +0100

    Add reflection/refraction to cubes

--- cubemaps_27/shaders/object.fs
+++ cubemaps_27/shaders/object.fs
@@ -1,11 +1,24 @@
 #version 460 core
 
-in vec2 TexCoords;
+in vec3 Normal;
+in vec3 Position;
+
 out vec4 FragColor;
 
-uniform sampler2D objectTexture;
+uniform samplerCube skyboxTexture;
+uniform vec3 cameraPos;
 
 void main()
 {
-    FragColor = texture(objectTexture, TexCoords);
+    // Reflection cubes
+    vec3 I = normalize(Position-cameraPos); // Camera position to fragment world position
+    vec3 R = reflect(I, normalize(Normal));
+    vec3 result = texture(skyboxTexture, R).rgb * vec3(1.0, 0.1, 0.1);
+    FragColor = vec4(result, 1.0);
+
+    // Refraction cubes
+    // float ratio = 1.0 / 1.52;
+    // vec3 I = normalize(Position-cameraPos); // Camera position to fragment world position
+    // vec3 R = refract(I, normalize(Normal), ratio);
+    // FragColor = vec4(texture(skyboxTexture, R).rgb, 1.0);
 }
--- cubemaps_27/src/main.cpp
+++ cubemaps_27/src/main.cpp
@@ -155,35 +155,35 @@ int main()
 
     float cube_vertices[] = {
         // Back face
-        -0.5f, -0.5f, -0.5f, 1.f, 0.f,
-        0.5f, -0.5f, -0.5f, 0.f, 0.f,
-        0.5f, 0.5f, -0.5f, 0.f, 1.f,
-        -0.5f, 0.5f, -0.5f, 1.f, 1.f,
+        -0.5f, -0.5f, -0.5f, 0.f, 0.f, -1.f,
+        0.5f, -0.5f, -0.5f, 0.f, 0.f, -1.f,
+        0.5f, 0.5f, -0.5f, 0.f, 0.f, -1.f,
+        -0.5f, 0.5f, -0.5f, 0.f, 0.f, -1.f,
         // Front face
-        -0.5f, -0.5f, 0.5f, 0.f, 0.f,
-        0.5f, -0.5f, 0.5f, 1.f, 0.f,
-        0.5f, 0.5f, 0.5f, 1.f, 1.f,
-        -0.5f, 0.5f, 0.5f, 0.f, 1.f,
+        -0.5f, -0.5f, 0.5f, 0.f, 0.f, 1.f,
+        0.5f, -0.5f, 0.5f, 0.f, 0.f, 1.f,
+        0.5f, 0.5f, 0.5f, 0.f, 0.f, 1.f,
+        -0.5f, 0.5f, 0.5f, 0.f, 0.f, 1.f,
         // Bottom face
-        -0.5f, -0.5f, -0.5f, 0.f, 0.f,
-        0.5f, -0.5f, -0.5f, 1.f, 0.f,
-        0.5f, -0.5f, 0.5f, 1.f, 1.f,
-        -0.5f, -0.5f, 0.5f, 0.f, 1.f,
+        -0.5f, -0.5f, -0.5f, 0.f, -1.f, 0.f,
+        0.5f, -0.5f, -0.5f, 0.f, -1.f, 0.f,
+        0.5f, -0.5f, 0.5f, 0.f, -1.f, 0.f,
+        -0.5f, -0.5f, 0.5f, 0.f, -1.f, 0.f,
         // Top face
-        -0.5f, 0.5f, -0.5f, 0.f, 1.f,
-        0.5f, 0.5f, -0.5f, 1.f, 1.f,
-        0.5f, 0.5f, 0.5f, 1.f, 0.f,
-        -0.5f, 0.5f, 0.5f, 0.f, 0.f,
+        -0.5f, 0.5f, -0.5f, 0.f, 1.f, 0.f,
+        0.5f, 0.5f, -0.5f, 0.f, 1.f, 0.f,
+        0.5f, 0.5f, 0.5f, 0.f, 1.f, 0.f,
+        -0.5f, 0.5f, 0.5f, 0.f, 1.f, 0.f,
         // Left face
-        -0.5f, -0.5f, -0.5f, 0.f, 0.f,
-        -0.5f, -0.5f, 0.5f, 1.f, 0.f,
-        -0.5f, 0.5f, 0.5f, 1.f, 1.f,
-        -0.5f, 0.5f, -0.5f, 0.f, 1.f,
+        -0.5f, -0.5f, -0.5f, -1.f, 0.f, 0.f,
+        -0.5f, -0.5f, 0.5f, -1.f, 0.f, 0.f,
+        -0.5f, 0.5f, 0.5f, -1.f, 0.f, 0.f,
+        -0.5f, 0.5f, -0.5f, -1.f, 0.f, 0.f,
         // Right face
-        0.5f, -0.5f, 0.5f, 0.f, 0.f,
-        0.5f, -0.5f, -0.5f, 1.f, 0.f,
-        0.5f, 0.5f, -0.5f, 1.f, 1.f,
-        0.5f, 0.5f, 0.5f, 0.f, 1.f
+        0.5f, -0.5f, 0.5f, 1.f, 0.f, 0.f,
+        0.5f, -0.5f, -0.5f, 1.f, 0.f, 0.f,
+        0.5f, 0.5f, -0.5f, 1.f, 0.f, 0.f,
+        0.5f, 0.5f, 0.5f, 1.f, 0.f, 0.f
     };
 
     unsigned int cube_indices[] = {
@@ -199,18 +199,6 @@ int main()
         16, 18, 19,
         20, 21, 22,
         20, 22, 23
-    };
-
-    float floor_vertices[] = {
-        -5.f, -0.5f, -5.f, 0.f, 2.f,
-        5.f, -0.5f, -5.f, 2.f, 2.f,
-        -5.f, -0.5f, 5.f, 0.f, 0.f,
-        5.f, -0.5f, 5.f, 2.f, 0.f,
-    };
-
-    unsigned int floor_indices[] = {
-        0, 3, 1,
-        0, 2, 3
     };
 
     float skybox_vertices[] = {
@@ -262,12 +250,8 @@ int main()
     };
 
     // -----------------------------------
-    // LOAD THE CUBE OBJECT TEXTURES
+    // unsigned int cubeTexture = loadTexture("../../assets/container.jpg");
 
-    unsigned int cubeTexture = loadTexture("../../assets/container.jpg");
-    unsigned int floorTexture = loadTexture("../../assets/metal.png");
-
-    // -----------------------------------
     // CUBEMAP
     std::vector<std::string> faces_path = {
         "../../assets/skybox/right.jpg",
@@ -281,7 +265,7 @@ int main()
     unsigned int cubemapTextureID = loadCubemap(faces_path);
 
     // -----------------------------------
-    unsigned int cubeVBO, cubeVAO, cubeEBO, floorVAO, floorVBO, floorEBO;
+    unsigned int cubeVBO, cubeVAO, cubeEBO;
     // CUBE OBJECT
     glGenVertexArrays(1, &cubeVAO);
     glGenBuffers(1, &cubeVBO);
@@ -291,30 +275,13 @@ int main()
     glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
     
     glBindVertexArray(cubeVAO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(3*sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
     glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeEBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_indices), cube_indices, GL_STATIC_DRAW);
-
-    // FLOOR OBJECT
-    glGenVertexArrays(1, &floorVAO);
-    glGenBuffers(1, &floorVBO);
-    glGenBuffers(1, &floorEBO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, floorVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(floor_vertices), floor_vertices, GL_STATIC_DRAW);
-    
-    glBindVertexArray(floorVAO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(3*sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, floorEBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(floor_indices), floor_indices, GL_STATIC_DRAW);
     
     // CUBEMAP
     unsigned int skyboxVBO, skyboxVAO, skyboxEBO;
@@ -347,7 +314,7 @@ int main()
     int modelLocation = glGetUniformLocation(objectShader.m_id, "model");
     int viewLocation = glGetUniformLocation(objectShader.m_id, "view");
     int projectionLocation = glGetUniformLocation(objectShader.m_id, "projection");
-    objectShader.SetInt("objectTexture", 0);
+    objectShader.SetInt("skyboxTexture", 0);
 
     Shader skyboxShader("../shaders/skybox.vs", "../shaders/skybox.fs");
     skyboxShader.SetInt("skyboxTexture", 0);
@@ -371,28 +338,15 @@ int main()
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), 800.f/600.f, 0.1f, 100.f);
         glm::mat4 view = glm::lookAt(camera.Position, camera.Position+camera.Front, camera.Up); 
         glm::mat4 skyboxView = glm::mat4(glm::mat3(view)); // Removing the translations in the view matrix
-        
-        // -----------------------------------
-        // CUBEMAP
-        glCullFace(GL_FRONT);
-        skyboxShader.Use();
-        glDepthMask(GL_FALSE);
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTextureID); 
-        glUniformMatrix4fv(projectionLocationSkybox, 1, GL_FALSE, glm::value_ptr(projection));
-        glUniformMatrix4fv(viewLocationSkybox, 1, GL_FALSE, glm::value_ptr(skyboxView));
-        glBindVertexArray(skyboxVAO);
-        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-        glDepthMask(GL_TRUE);
-        glCullFace(GL_BACK);
 
         // -----------------------------------
         // CUBE OBJECT
         objectShader.Use();
         glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(projection)); 
         glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
+        objectShader.SetVec3("cameraPos", camera.Position);
         glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, cubeTexture); 	
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTextureID); 	
 
         glBindVertexArray(cubeVAO);
         glm::mat4 cube_model = glm::mat4(1.f);
@@ -406,13 +360,20 @@ int main()
         glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
         // -----------------------------------
-        // FLOOR OBJECT
-        glBindTexture(GL_TEXTURE_2D, floorTexture);
-
-        glBindVertexArray(floorVAO);
-        glm::mat4 floor_model = glm::mat4(1.f);
-        glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(floor_model));
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // CUBEMAP
+        glCullFace(GL_FRONT);
+        glDepthFunc(GL_LEQUAL);
+        skyboxShader.Use();
+        // glDepthMask(GL_FALSE);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTextureID); 
+        glUniformMatrix4fv(projectionLocationSkybox, 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(viewLocationSkybox, 1, GL_FALSE, glm::value_ptr(skyboxView));
+        glBindVertexArray(skyboxVAO);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        // glDepthMask(GL_TRUE);
+        glCullFace(GL_BACK);
+        glDepthFunc(GL_LESS);
 
         // -----------------------------------
 
@@ -422,8 +383,6 @@ int main()
 
     glDeleteVertexArrays(1, &cubeVAO);
     glDeleteBuffers(1, &cubeVBO);
-    glDeleteVertexArrays(1, &floorVAO);
-    glDeleteBuffers(1, &floorVBO);
     glDeleteVertexArrays(1, &skyboxVAO);
     glDeleteBuffers(1, &skyboxVBO);
     
